61bee12f1d8d233a6cbf98ef758ab6079b5edce9
Gaston Michelotti <gastonmichelotti@gmail.com>
2022-02-15 16:27:03 -0300

    FIX FORANEA

---
+++ Migrations/20220215192354_firstSE.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace netCoreNew.Migrations
+{
+    public partial class firstSE : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+          
+
+            
+
+            
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+           
+           
+        }
+    }
+}
--- Models/Keep/Proyecto.cs
+++ Models/Keep/Proyecto.cs
@@ -22,10 +22,10 @@ namespace netCoreNew.Models
         [NotMapped]
         public string ItemsLoad { get; set; }
 
-        //[NotMapped]
-        //public ICollection<Recuento> Recuentos { get; set; }
+        [NotMapped]
+        public ICollection<Recuento> Recuentos { get; set; }
 
-        public Recuento[] Items { get; set; }
+        //public Recuento[] Items { get; set; }
 
 
 
